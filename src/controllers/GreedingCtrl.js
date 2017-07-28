da1a6b3f41a2dd21253ba9dc60c21ac40d99279b
Akihiro OKAYAMA <akihiroxob@yahoo.co.jp>
2017-07-28 22:03:57 +0900

    会話文章を追加

--- src/controllers/GreedingCtrl.js
+++ src/controllers/GreedingCtrl.js
@@ -5,13 +5,96 @@ export default controller => {
         'direct_message,direct_mention,mention',
         (bot, message) => {
             const text = [
-                'おはようございます。',
+                'おはようございます',
                 '朝ですか、おはようございます',
-                'お元気そうで何よりです',
                 '...zZＺ',
             ];
             bot.reply(message, text[(Math.random() * text.length)|0]);
         }
     );
 
+    controller.hears(
+        ["こんにちは", "こんにちわ"],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            const text = [
+                'こんにちは',
+                'こんにちは、お昼はもう食べましたか？',
+                'こんにちは。お昼を食べた後は眠くなりますね。',
+            ];
+            bot.reply(message, text[(Math.random() * text.length)|0]);
+        }
+    );
+
+    controller.hears(
+        ["さよなら", "おやすみ"],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            const text = [
+                'お疲れ様でした',
+                'お疲れ様でした、また明日お会いしましょう',
+                '今日も1日大変でしたね。お疲れ様でした。',
+            ];
+            bot.reply(message, text[(Math.random() * text.length)|0]);
+        }
+    );
+
+    controller.hears(
+        ["おつかれ", "お疲れ"],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            const text = [
+                'いえ、私はそんなに大したことは',
+                'ありがとうございます',
+            ];
+            bot.reply(message, text[(Math.random() * text.length)|0]);
+        }
+    );
+
+    controller.hears(
+        ["さすが", "すごい"],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            const text = [
+                'いえ、滅相もない',
+                'そう言って頂けるとありがたいです'
+            ];
+            bot.reply(message, text[(Math.random() * text.length)|0]);
+        }
+    );
+
+    controller.hears(
+        ["ありがとう"],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            const text = [
+                'いえ、私はそんなに大したことは',
+            ];
+            bot.reply(message, text[(Math.random() * text.length)|0]);
+        }
+    );
+
+    controller.hears(
+        ["疲れた", "つかれた"],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            const text = [
+                'お疲れ様です。',
+                'あまり無理せず体を労って下さい',
+            ];
+            bot.reply(message, text[(Math.random() * text.length)|0]);
+        }
+    );
+
+    controller.hears(
+        ["ねむい", "眠い"],
+        'direct_message,direct_mention,mention',
+        (bot, message) => {
+            const text = [
+                'お疲れ様です。',
+                'そろそろ寝てはいかがですか？',
+            ];
+            bot.reply(message, text[(Math.random() * text.length)|0]);
+        }
+    );
 };
